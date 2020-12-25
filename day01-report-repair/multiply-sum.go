98255910dce096f978899969a56ab693eb9ae9c7
Remco Kranenburg <remco@burgsoft.nl>
2020-12-25 15:06:53 +0100

    Implement Day 1, Part 1: Report Repair

---
+++ day01-report-repair/multiply-sum.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+)
+
+func main() {
+    var numbers []int64
+
+    scanner := bufio.NewScanner(os.Stdin)
+
+    for scanner.Scan() {
+        numberText := scanner.Text()
+	number, _ := strconv.ParseInt(numberText, 10, 0)
+	numbers = append(numbers, number)
+    }
+
+    for i, number1 := range numbers {
+        for j, number2 := range numbers {
+	    if i < j && number1 + number2 == 2020 {
+                fmt.Println(number1 * number2)
+	    }
+	}
+    }
+}
