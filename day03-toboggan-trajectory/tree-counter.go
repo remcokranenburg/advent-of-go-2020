a8b28b51a4e121867328d5925c94a45f8f747391
Remco Kranenburg <remco@burgsoft.nl>
2021-01-14 11:58:08 +0100

    Day 3, Part 1

---
+++ day03-toboggan-trajectory/tree-counter.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+    "strconv"
+)
+
+func main() {
+    filename := os.Args[1]
+    numRight, _ := strconv.ParseInt(os.Args[2], 10, 0)
+    numDown, _ := strconv.ParseInt(os.Args[3], 10, 0)
+
+    content, _ := ioutil.ReadFile(filename)
+
+    var field [][]bool
+    field = append(field, []bool{})
+
+    for _, value := range(content) {
+        if value == '\n' {
+            field = append(field, []bool{})
+        } else if value == '#' {
+            row := len(field) - 1
+            field[row] = append(field[row],true)
+        } else if value == '.' {
+            row := len(field) - 1
+            field[row] = append(field[row], false)
+        } else {
+            fmt.Printf("Warning: unexpected character: %s\n", value)
+        }
+    }
+
+    numTrees := 0
+
+    for y, x := 0, 0; y < len(field) - 1; y, x = y + int(numDown), x + int(numRight) {
+        if field[y][x % len(field[y])] {
+            numTrees += 1
+        }
+    }
+
+    fmt.Println(numTrees)
+}
