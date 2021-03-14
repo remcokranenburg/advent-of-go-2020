5d5331a9b77d450a0784e6cd88994748d33946b1
Remco Kranenburg <remco@burgsoft.nl>
2021-03-14 17:46:35 +0100

    WIP

--- day11-seating-system/seating.go
+++ day11-seating-system/seating.go
@@ -22,21 +22,61 @@ func copySeating(seats [][]rune) [][]rune {
     return newSeats
 }
 
+func clamp(seats [][]rune, row, seat int) rune {
+    if row < 0 || row >= len(seats) {
+        return 'X'
+    }
+
+    if seat < 0 || seat >= len(seats[row]) {
+        return 'X'
+    }
+
+    return seats[row][seat]
+}
+
+func numNeighbors(seats [][]rune, row, seat int) int {
+    result := 0
+
+    for i := row - 1; i <= row + 1; i++ {
+        for j := seat - 1; j <= seat + 1; j++ {
+            if i != row && j != seat && clamp(seats, i, j) == '#' {
+                result++
+            }
+        }
+    }
+
+    return result
+}
+
+func numSeated(seats [][]rune) int {
+    result := 0
+
+    for _, row := range seats {
+        for _, seat := range row {
+            if seat == '#' {
+                result++
+            }
+        }
+    }
+
+    return result
+}
+
 func predictSeating(seats [][]rune) [][]rune {
     newSeats := [][]rune{}
 
-    for _, row := range seats {
+    for i, row := range seats {
         newRow := []rune{}
 
-        for _, seat := range row {
+        for j, seat := range row {
             newSeat := 'X'
 
-            if seat == 'L' {
+            if seat == 'L' && numNeighbors(seats, i, j) == 0 {
                 newSeat = '#'
-            } else if seat == '#' {
+            } else if seat == '#' && numNeighbors(seats, i, j) >= 4 {
                 newSeat = 'L'
-            } else if seat == '.' {
-                newSeat = '.'
+            } else {
+                newSeat = seat
             }
 
             newRow = append(newRow, newSeat)
@@ -46,6 +86,36 @@ func predictSeating(seats [][]rune) [][]rune {
     }
 
     return newSeats
+}
+
+func printSeats(seats [][]rune) {
+    for _, row := range seats {
+        for _, seat := range row {
+            fmt.Printf("%c", seat)
+        }
+
+        fmt.Println()
+    }
+}
+
+func seatingIsEqual(a, b [][]rune) bool {
+    if len(a) != len(b) {
+        return false
+    }
+
+    for i, _ := range a {
+        if len(a[i]) != len(b[i]) {
+            return false
+        }
+
+        for j, _ := range a[i] {
+            if a[i][j] != b[i][j] {
+                return false
+            }
+        }
+    }
+
+    return true
 }
 
 func main() {
@@ -63,8 +133,17 @@ func main() {
         seats = append(seats, row)
     }
 
-    newSeats := predictSeating(seats)
+    printSeats(seats)
+    fmt.Println()
 
-    fmt.Println(seats)
-    fmt.Println(newSeats)
+    previousSeats := [][]rune{}
+
+    for i := 0; !seatingIsEqual(previousSeats, seats); i++ {
+        previousSeats = seats
+        seats = predictSeating(seats)
+        printSeats(seats)
+        fmt.Println()
+    }
+
+    fmt.Println(numSeated(seats))
 }
